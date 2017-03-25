96ff23c43bd3d2865b6a13d81c0b05b0f766440d
jose luis moragas garcia <jl.moragas@hotmail.es>
2017-03-25 16:43:59 +0100

    Including constructors.

--- Sudoku.java
+++ Sudoku.java
@@ -6,6 +6,31 @@ package com.joselemg.sudokupkg;
  * @author Josele
  * @version 23.03.2017
  */
+
 public class Sudoku {
-    private static Casilla [][] casillas = null;
+    private static Cell[][] cells = null;
+    private StateGame stateGame = null;
+    private int n;
+
+    /**
+     * Constructor:  N xN Matrix of cells.
+     *
+     * @param n
+     */
+    public Sudoku(int n) {
+        this.n = n;
+        this.cells = new Cell[n][n];
+        for (int x = 0; x < n; x++) {
+            for (int y = 0; y < n; y++) {
+                cells[x][y] = new Cell(x, y);
+            }
+        }
+        this.stateGame = StateGame.STARTED;
+    }
+
+    public Sudoku(Cell[][] cells, int n) {
+        this.n = n;
+        this.cells = cells;
+        this.stateGame = StateGame.STARTED;
+    }
 }
