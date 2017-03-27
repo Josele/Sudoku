1c882407208b3b8141a3a7035c15ca91000ba02e
jose luis moragas garcia <jl.moragas@hotmail.es>
2017-03-28 01:21:18 +0200

    Renaming class

---
+++ SudokuSolver.java
@@ -0,0 +1,26 @@
+
+/**
+ * @author Josele
+ * @version 27.03.2017
+ */
+public class SudokuSolver {
+
+
+    public static void main(String[] args) throws Exception {
+        SudokuGame oneGame;
+        try {
+            if (args.length == 0) {
+                System.out.println("Missing input file.");
+            } else {
+                oneGame = new SudokuGame(args[0]);
+                System.out.println("Entered sudoku:");
+                oneGame.printStateGame();
+                oneGame.solve();
+                System.out.println("Output sudoku:");
+                oneGame.printStateGame();
+            }
+        } catch (RuntimeException e) {
+            System.err.println(e);
+        }
+    }
+}
